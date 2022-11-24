c95848b0215be6dece23759ff9770a283bd3412a
Paolo224 <picariellopaolo224@gmail.com>
2022-11-24 15:19:50 +0100

    Completato gioco dei dadi

--- dati.js
+++ dati.js
@@ -6,25 +6,32 @@ let risultatoUtente;
 
 let risultatoPc;
 
-for( i = 1; i <= 6; i = i + 1){
-    randomNumberUtente = Math.floor(Math.random() * 7);
-    risultatoUtente = document.getElementById('output-1').innerHTML = "Il tuo numero: " + randomNumberUtente;
+const button = document.getElementById('button-1');
 
-}
+button.addEventListener('click' , function(){
+    for( i = 1; i <= 6; i = i + 1){
+        randomNumberUtente = Math.floor(Math.random() * 7);
+        risultatoUtente = document.getElementById('output-1').innerHTML = "Il tuo numero: " + randomNumberUtente;
+    }
+    
+    for( i = 1; i <= 6; i = i + 1){
+        randomNumberPc = Math.floor(Math.random() * 7);
+        risultatoPc = document.getElementById('output-2').innerHTML = "Il mio numero: " + randomNumberPc;
+    }
+    
+    let vincitore;
+    
+    
+    if ( randomNumberPc === randomNumberUtente ){
+        vincitore = "Pareggio!!!"
+    }
+    else if (randomNumberPc < randomNumberUtente){
+        vincitore = "Hai vinto tu!... TUTTA FORTUNA!!!!"
+    }
+    else{
+        vincitore = "Ho vinto io!!!!! TIÉ!!!!"
+    }
+    
+    document.getElementById('vittoria').innerHTML = vincitore;
+})
 
-for( i = 1; i <= 6; i = i + 1){
-    randomNumberPc = Math.floor(Math.random() * 7);
-    risultatoPc = document.getElementById('output-2').innerHTML = "Il mio numero: " + randomNumberPc;
-
-}
-
-let vincitore;
-
-if (risultatoPc > risultatoUtente){
-    vincitore = "Hai vinto tu!...   TUTTA FORTUNA!!!!"
-}
-else{
-    vincitore = "Ho vinto io!!!!!   TIÉ!!!!"
-}
-
-document.getElementById('vittoria').innerHTML = vincitore;
